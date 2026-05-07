8632daece70f0f6e86a4ef4a7224bba61cc25d37
rrahulvishwakarma007-lgtm <rrahulvishwakarma007@gmail.com>
2026-05-07 16:05:40 +0530

    Update App.tsx

--- agon-agent_1-8e508fc8/src/App.tsx
+++ agon-agent_1-8e508fc8/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AdminDashboard from './pages/AdminDashboard';
+import SurveyPage from './pages/survey';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/survey" element={<SurveyPage />} />
       </Routes>
     </BrowserRouter>
   );
